3c0721cad6bb1d8af63f27a7f6ad105d4a2a2a0f
Michel Bystranowski <michel@vertige.org>
2016-06-01 15:44:22 +0200

    commit initial - généré par la Fabrique

---
+++ analyclic_partout_pipelines.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Utilisations de pipelines par Analyclic Partout
+ *
+ * @plugin     Analyclic Partout
+ * @copyright  2016
+ * @author     Michel @ Vertige ASBL
+ * @licence    GNU/GPL
+ * @package    SPIP\Analyclic_partout\Pipelines
+ */
---
+++ lang/paquet-analyclic_partout_fr.php
@@ -0,0 +1,10 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'analyclic_partout_description' => '',
+	'analyclic_partout_nom' => 'Analyclic Partout',
+	'analyclic_partout_slogan' => 'compter les téléchargements partout',
+);
