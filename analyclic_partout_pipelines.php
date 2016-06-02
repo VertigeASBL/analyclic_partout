bff221f96051045159a9da9e0881c04348ba1434
Michel Bystranowski <michel@vertige.org>
2016-06-02 09:19:01 +0200

    les fonctions utilisées dans declarer table interfaces doivent être déclarées plus tôt
    Et ne peuvent donc pas être dans un fichier _fonctions.php

--- analyclic_partout_pipelines.php
+++ analyclic_partout_pipelines.php
@@ -47,3 +47,45 @@ function analyclic_partout_declarer_tables_interfaces($interfaces) {
 
 	return $interfaces;
 }
+
+/**
+ * Ajouter un traitement automatique sur une balise
+ *
+ * On peut restreindre l'application du traitement au balises appelées dans un
+ * type de boucle via le paramètre optionnel $table.
+ *
+ * @param array $interfaces
+ *    Les interfaces du pipeline declarer_tables_interfaces
+ * @param string $traitement
+ *    Un format comme pour sprintf, dans lequel le compilateur passera la valeur de la balise
+ * @param string $balise
+ *    Le nom de la balise à laquelle on veut appliquer le traitement
+ * @param string $table (optionnel)
+ *    Un type de boucle auquel on veut restreindre le traitement.
+ */
+function anaclic_ajouter_traitement_automatique($interfaces, $traitement, $balise, $table = 0) {
+
+	$table_traitements = $interfaces['table_des_traitements'];
+
+	if (! isset($table_traitements[$balise])) {
+		$table_traitements[$balise] = array();
+	}
+
+	/* On essaie d'être tolérant sur le nom de la table */
+	if ($table) {
+		include_spip('base/objets');
+		$table = table_objet($table);
+	}
+
+	if (isset($table_traitements[$balise][$table])) {
+		$traitement_existant = $table_traitements[$balise][$table];
+	}
+
+	if (!isset($traitement_existant) or (! $traitement_existant)) {
+		$traitement_existant = '%s';
+	}
+
+	$interfaces['table_des_traitements'][$balise][$table] = sprintf($traitement, $traitement_existant);
+
+	return $interfaces;
+}
